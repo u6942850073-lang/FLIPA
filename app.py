4e825ce932ead5b1aa937d88db98aac8022042e1
Cutileiro <j.cutileiro@sap.com>
2026-06-22 20:18:01 +0100

    Adicionar allow_unsafe_werkzeug para produção

--- app.py
+++ app.py
@@ -404,4 +404,4 @@ def on_disconnect():
 if __name__ == "__main__":
     db.init_db()
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, debug=False, host="0.0.0.0", port=port)
+    socketio.run(app, debug=False, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
